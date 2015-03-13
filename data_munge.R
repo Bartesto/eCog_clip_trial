81f18635e8b4ad52cad567e319f2de4a3f54f860
Bartesto <barthuntley@iinet.net.au>
2015-03-13 08:40:46 +0800

    Added comments and formatted txt files for Python - i.e. no quotes and backslashes

--- data_munge.R
+++ data_munge.R
@@ -5,23 +5,23 @@
 ## 1. this script
 ## 2. a folder called "input" containg eCog outputs
 ## 3. a folder called "clip" containing ALL UM tiles for use in clipping eCog outputs
-## 4. a file geodatabse for clipped outputs
+## 4. a file geodatabse for clipped outputs - can be named anything.
 
 setwd("your file path here")
 
 ##Obtain top level file path
 fp <- getwd()
-fp <- gsub("/", "\\\\", fp, fixed = TRUE)
+fp <- gsub("/", "\\", fp, fixed = TRUE) # Need 2 backslashes to create 1. One is an escape character.
 
 ##Obtain file geo database name
 gdb <- dir(pattern = "\\.gdb$")
 
 ##Get list of input files
 input <- sort(list.files("./input", pattern = "\\.shp$"))#basename
-input_files <- paste0(fp, "\\\\", "input", "\\\\", sort(list.files("./input", pattern = "\\.shp$")))
+input_files <- paste0(fp, "\\", "input", "\\", sort(list.files("./input", pattern = "\\.shp$")))
 
 ##Get list of clip files
-clip_files <- paste0(fp, "\\\\", "clip", "\\\\", sort(list.files("./clip", pattern = "\\.shp$")))
+clip_files <- paste0(fp, "\\", "clip", "\\", sort(list.files("./clip", pattern = "\\.shp$")))
 
 
 ## Get common identifier for output name
@@ -32,7 +32,7 @@ split <- as.data.frame(matrix(unlist(strsplit(input, split = "\\.")), nrow=lgth,
 
 ## Create name for output name
 name_no_shp <- substr(split[,1], 1, 35)## hard coded to match input string format
-out_name <- paste0(fp, "\\\\", gdb, "\\\\", "UM", "_", name_no_shp)
+out_name <- paste0(fp, "\\", gdb, "\\", "UM", "_", name_no_shp)
 
 
 ## Get common identifier to search UM tiles
@@ -46,7 +46,9 @@ clips <- sapply(1:lgth, function(i){clip_files[pos_ind[i]]})
 df <- as.data.frame(cbind(id = id, input = input_files, clip = clips,
                           output = out_name), stringsAsFactors = FALSE)
 
-
-
-
+## Write out txt files for Python script parameters. Note no quotes and when Python imports
+## the text files it doubles the back slashes.
+write.table(df$input, "input.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(df$clip, "UMclip.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(df$output, "output.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
